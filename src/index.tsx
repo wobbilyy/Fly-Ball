e2112064f70cf2669c4d6bf119ae1a8d9118ec5d
Bobby Wang <bobbywang0@gmail.com>
2023-09-13 17:22:08 -0700

    Small bugs in execution

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot }  from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.tsx';
-import Routes from './Routes.tsx';
+import Routes from './routes.tsx';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
